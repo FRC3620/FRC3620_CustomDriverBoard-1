668db930a42f8bba2b778b03ef273cb7acdd2a05
Kai3620 <kaiborah@yahoo.com>
2016-02-24 23:15:30 -0500

    Testing push functionality

---
+++ CustomDriverBoard_v0.1/CustomDriverBoard_v0.1.ino
@@ -0,0 +1,46 @@
+// v0.1
+// 
+// Sketch to connect a custom driver board
+// as an emulated joystick.
+// 
+// by Kai Borah (Team 3620)
+// 
+// Joystick library by Matthew Heironimus
+//---------------------------------------------
+
+#include <Joystick.h>
+
+void setup() {
+  // Initialize Button Pins
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
+
+  // Initialize Joystick Library
+  Joystick.begin();
+}
+
+// Constant that maps the phyical pin to the joystick button. 
+// Put the first pin number here.
+const int pinToButtonMap = 2;
+
+// Put number of buttons and last state for each one here.
+int lastButtonState[5] = {0,0,0,0,0};
+
+void loop() {
+
+  // Read pin values
+  for (int index = 0; index < 5; index++)
+  {
+    int currentButtonState = !digitalRead(index + pinToButtonMap);
+    if (currentButtonState != lastButtonState[index])
+    {
+      Joystick.setButton(index, currentButtonState);
+      lastButtonState[index] = currentButtonState;
+    }
+  }
+
+  delay(50);
+}
