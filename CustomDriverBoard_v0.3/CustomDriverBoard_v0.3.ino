4d2cb69154c3cb087787f57ecc815fed241580c3
Kai3620 <kaiborah@yahoo.com>
2016-02-26 14:01:33 -0500

    Conveniently moved toggle mode setting to the top.

--- CustomDriverBoard_v0.3/CustomDriverBoard_v0.3.ino
+++ CustomDriverBoard_v0.3/CustomDriverBoard_v0.3.ino
@@ -11,6 +11,9 @@
 
 #include <Joystick.h>
 
+// Enable or disable button toggling.
+int toggleMode = true;
+
 void setup() {
   pinMode(2, INPUT_PULLUP);
   pinMode(3, INPUT_PULLUP);
@@ -36,7 +39,6 @@ const int pinToButtonMap = 2;
 // Put number of buttons and last state for each one here.
 int lastButtonState[5] = {false,false,false,false,false};
 
-int toggleMode = true;
 
 void loop() {
 
@@ -45,7 +47,7 @@ void loop() {
     int currentButtonState = !digitalRead(index + pinToButtonMap);
     if (currentButtonState != lastButtonState[index]) {
       // Physical button state changed.
-      if (toggleMode) {
+      if (toggle mode) {
         if (currentButtonState) {
           // Physical button is depressed.
           for (int j = 0; j < 5; j++) {
